fe7fd3f6509913c9957de5995d29f184677897ec
Christian Murata <chris_murata@hotmail.com>
2022-01-18 22:16:53 -0300

    feat(home): presentation section

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Flex, Stack, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Image, Stack, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 const Navbar = (props) => {
@@ -20,7 +20,21 @@ const Navbar = (props) => {
             alignItems={'center'}
             justifyContent={'space-between'}
           >
-            <Box>Christian Murata</Box>
+            
+            <Flex
+              alignItems={'center'}
+              justifyContent={'start'}
+            >
+              <Image 
+                src='/images/rubiks.png' 
+                alt='logo'
+                width={5} 
+                height={5}
+              />
+              <Text fontWeight='bold' ml='1'>
+                Christian Murata
+              </Text>
+            </Flex>
 
             <Flex alignItems={'center'}>
               <Stack direction={'row'} spacing={7}>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,69 @@
-import { Box, Container, useColorModeValue } from "@chakra-ui/react";
-import Page from "../components/page"
+  import { AddIcon } from "@chakra-ui/icons";
+import { Badge, Box, Container, Flex, Heading, Image, Text, useColorModeValue } from "@chakra-ui/react";
+  import Page from "../components/page"
 
-const Home = () => {
-  return (
-    <Page>
-      <Container w='100%'>
-        <Box
-          p='3'
-          borderRadius='lg'
-          textAlign='center'
-          bg={useColorModeValue('#ffffff', '#323232')}
-        >
-          <h1>Hello from home page!</h1>
-        </Box>
-      </Container>
-    </Page>
-  );
-}
+  const Home = () => {
+    return (
+      <Page>
+        <Container w='100%'>
+          <Box
+            p='3'
+            borderRadius='lg'
+            textAlign='center'
+            mb='6'
+            bg={useColorModeValue('#ffffff', '#323232')}
+          >
+            <h1>Desenvolvedor e um fanático por puzzles nas horas vagas!</h1>
+          </Box>
 
-export default Home;
+          <Box display={{ md: 'flex' }}>
+            <Box textAlign='center'>
+              <Image
+                borderRadius='full'
+                maxWidth='100px'
+                src='/images/murata.jpg'
+                alt='Christian Murata'
+                borderStyle='solid'
+                borderWidth={3}
+                display='inline-block'
+                borderColor={useColorModeValue('#ffffff', '#323232')}
+              />
+            </Box>
+
+            <Flex alignItems={'center'}>
+              <Box ml='4'>
+                <Heading fontWeight='bold'>
+                  Christian Murata
+                </Heading>
+
+                <Text fontSize='sm' ml={1}>
+                  Desenvolvedor
+
+                  <Badge ml='1' colorScheme='blue'>
+                    PHP
+                  </Badge>
+                  <Badge ml='1' colorScheme='red'>
+                    LARAVEL
+                  </Badge>
+                  <Badge ml='1' colorScheme='yellow'>
+                    JS
+                  </Badge>
+                  <Badge ml='1' colorScheme='green'>
+                    Node
+                  </Badge>
+                  <Badge ml='1' colorScheme='orange'>
+                    Java
+                  </Badge>
+                  <Badge ml='1'>
+                    <AddIcon />
+                  </Badge>
+                </Text>
+              </Box>
+            </Flex>
+          </Box>
+        </Container>
+      </Page>
+    );
+  }
+
+  export default Home;
